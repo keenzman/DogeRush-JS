4cf231fcd2370da494b2bb6092c8ddefc139bdfc
keenzman <szaman_03@hotmail.co.uk>
2021-05-26 04:11:52 +0100

    Setup files

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector(".canvas__container");
+const ctx = canvas.getContext("2d");
+canvas.width = width.innerWidth;
+canvas.height = height.innerHeight;
+
+let size = 0;
+
+function animate() {
+  ctx.fillStyle = "red";
+}
