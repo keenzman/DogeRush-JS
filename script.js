e1b772bda3eb6e52d32372f351d4af34f4afffc0
keenzman <szaman_03@hotmail.co.uk>
2021-05-26 04:25:36 +0100

    Try to get circle on screen

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const canvas = document.querySelector(".canvas__container");
+const canvas = document.getElementById("#canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = width.innerWidth;
-canvas.height = height.innerHeight;
+// canvas.width = 850;
+// canvas.height = 450;
 
 let size = 0;
 
 function animate() {
-  ctx.fillStyle = "red";
+  // ctx.fillStyle = "red";
+  // ctx.beginPath();
+  // ctx.arc(100, 300, size, 0, Math.PI * 2);
+  // ctx.closePath();
+  // ctx.fill();
+  // ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  requestAnimationFrame(animate);
 }
+animate();
