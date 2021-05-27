d67e301b974b5c289cab00fa0bd96ee68bf63d47
keenzman <szaman_03@hotmail.co.uk>
2021-05-27 21:28:41 +0100

    refactor var names to be clearer

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ let canvasPosition = canvas.getBoundingClientRect();
 // console.log(canvasPosition);
 
 const mouse = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
+  horizontalPos: canvas.width / 2,
+  verticalPos: canvas.height / 2,
   click: false,
 };
 
 canvas.addEventListener("mousedown", function (event) {
   mouse.click = true;
   // getting the x,y coords from the bounded canvas positions
-  mouse.x = event.x - canvasPosition.left;
-  mouse.y = event.y - canvasPosition.top;
+  mouse.horizontalPos = event.x - canvasPosition.left;
+  mouse.verticalPos = event.y - canvasPosition.top;
   // console.log(mouse.x, mouse.y)
 });
 
@@ -38,8 +38,8 @@ class Player {
        * It will start moving towards the mouse coords, when the vals are different 
          than the mouse obj coords (on lines 17 and 18) on first page load.
        */
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
+    this.xAxis = canvas.width / 2;
+    this.yAxis = canvas.height / 2;
     this.radius = 50;
     this.angle = 0;
     this.frameX = 0;
@@ -52,28 +52,28 @@ class Player {
   }
   // update player pos
   update() {
-    const dx = this.x - mouse.x;
-    const dy = this.y - mouse.y;
+    const dx = this.xAxis - mouse.horizontalPos;
+    const dy = this.yAxis - mouse.verticalPos;
     // We want both x and y to change, so no else conditions
     // The 30 is used to stop the player teleporting instantly. Basically acts as delay.
-    if (mouse.x != this.x) {
-      this.x -= dx / 5;
+    if (mouse.horizontalPos != this.xAxis) {
+      this.xAxis -= dx / 5;
     }
-    if (mouse.y != this.y) {
-      this.y -= dy / 5;
+    if (mouse.verticalPos != this.yAxis) {
+      this.yAxis -= dy / 5;
     }
   }
   draw() {
     if (mouse.click) {
       ctx.lineWidth = 0.2;
       ctx.beginPath();
-      ctx.moveTo(this.x, this.y);
-      ctx.lineTo(mouse.x, mouse.y);
+      ctx.moveTo(this.xAxis, this.yAxis);
+      ctx.lineTo(mouse.horizontalPos, mouse.verticalPos);
       ctx.stroke();
     }
     ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.xAxis, this.yAxis, this.radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
   }
@@ -84,24 +84,24 @@ const player = new Player();
 const coinsArr = [];
 class Coin {
   constructor() {
-    this.x = Math.random() * canvas.width;
+    this.xAxis = Math.random() * canvas.width;
     // The coins rise up from the bottom edge of canvas + 100
-    this.y = canvas.height + 100;
+    this.yAxis = canvas.height + 100;
     this.radius = 50;
     this.speed = Math.random() * 5 + 1;
     this.distance;
     this.counted = false;
   }
   update() {
-    this.y -= this.speed;
-    const dx = this.x - player.x;
-    const dy = this.y - player.y;
+    this.yAxis -= this.speed;
+    const dx = this.xAxis - player.xAxis;
+    const dy = this.yAxis - player.yAxis;
     this.distance = Math.sqrt(dx * dx + dy * dy);
   }
   draw() {
     ctx.fillStyle = "goldenrod";
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(this.xAxis, this.yAxis, this.radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
     ctx.stroke();
@@ -119,6 +119,7 @@ function handleCoin() {
     coinsArr[i].update();
     coinsArr[i].draw();
   }
+
   for (let i = 0; i < coinsArr.length; i++) {
     // Note that the 0 - bit is to stop the coins from disappearing suddenly
     if (coinsArr[i].y < 0 - coinsArr[i].radius * 2) {
@@ -146,7 +147,7 @@ function animate() {
   player.draw();
   // score placeholder
   ctx.fillStyle = "black";
-  ctx.fillText("score: " + score, 10, 50);
+  ctx.fillText("Score: " + score, 550, 50);
   gameFrame++;
   // console.log(gameFrame);
   requestAnimationFrame(animate);
